5c1b1c4a28f30a74b6b867393b2fedf4eb1e5a3e
cchantep <chantepie@altern.org>
2014-10-29 16:59:43 +0100

    Spanish localization

---
+++ lang/jquery.signfield-es.js
@@ -0,0 +1,9 @@
+/**
+ * Spanish localized messages.
+ */
+var signField_I18N = {
+    'sketch.label': "Firma manuscrita",
+    'sketch.clear': "Borrar",
+    'file.label': "Elija un archivo",
+    'file.error.maxSize': "Tamaño máximo es excedió: {0} > {1}"    
+};
